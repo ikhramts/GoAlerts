a034361ee6a7ae3d1d438588240364fb34874dad
ikhramts <junkdump3001@hotmail.com>
2014-08-17 12:09:21 -0400

    Initial commit.

---
+++ GoAlertsServer/GoAlertsServer/Model/GoStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoAlertsServer.Model {
+    public class GoStatus {
+        public List<LineStatus> TrainStatuses { get; private set; }
+        public List<LineStatus> BusStatuses { get; private set; }
+    }
+}
---
+++ GoAlertsServer/GoAlertsServer/Model/LineStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoAlertsServer.Model {
+    public class LineStatus {
+        public string LineName { get; set; }
+        public bool IsOnTime { get; set; }
+        public int SecondsLate { get; set; }
+        public string Message { get; set; }
+    }
+}
